0eac193c442ccbb89f7d4cca77a0a44df1da9a6f
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-03-04 16:08:36 -0500

    Get city and use to fetch to API on click of submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,20 @@
 // TODO: Add in ability to choose from a list of cities, ex: London US vs London UK
 
-var requestUrl =
-  "http://api.openweathermap.org/geo/1.0/direct?q=" +
-  "Cary" +
-  "&APPID=6aa641400e3e28191b162c454ad4f43e";
-fetch(requestUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+$("#city-search-btn").click(test);
+
+function test(event) {
+  event.preventDefault();
+  var city = $("#city-entry").val();
+  console.log(city);
+  var requestUrl =
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+    city +
+    "&APPID=6aa641400e3e28191b162c454ad4f43e";
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
