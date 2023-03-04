57c35375a74db12fdf8e133a12a24166d5b67cbe
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-03-04 15:49:24 -0500

    Create request to OpenWeather's Geocoding API to get coordinates based on city name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+// TODO: Add in ability to choose from a list of cities, ex: London US vs London UK
+
+var requestUrl =
+  "http://api.openweathermap.org/geo/1.0/direct?q=" +
+  "Cary" +
+  "&APPID=6aa641400e3e28191b162c454ad4f43e";
+fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
