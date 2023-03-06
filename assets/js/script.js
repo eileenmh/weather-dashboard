4d268814eb52c91b3162120538c8bc41eb97d636
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-03-05 23:04:17 -0500

    Get lat and lon from geocode request and use to request 5 day weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 // TODO: Add in ability to choose from a list of cities, ex: London US vs London UK
 
+function City(latitude, longitude) {
+  this.latitude = latitude;
+  this.longitude = longitude;
+}
+
 $("#city-search-btn").click(test);
 
 function test(event) {
@@ -15,6 +20,18 @@ function test(event) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
+      var requestUrl =
+        "http://api.openweathermap.org/data/2.5/forecast?lat=" +
+        data[0].lat +
+        "&lon=" +
+        data[0].lon +
+        "&APPID=6aa641400e3e28191b162c454ad4f43e";
+      fetch(requestUrl)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+        });
     });
 }
