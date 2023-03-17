4740baf822e1ede0b2596783838ca0c7407122b2
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-03-17 14:32:00 -0400

    Create dropdown options in location search bar using api results

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,75 @@
-// TODO: Add in ability to choose from a list of cities, ex: London US vs London UK
 const apiKey = "6aa641400e3e28191b162c454ad4f43e";
 
-function fetchCoordinatesUrl(city) {
-  url =
-    "http://api.openweathermap.org/geo/1.0/direct?q=" +
-    city +
-    "&APPID=" +
-    apiKey;
-  return url;
+function fetchCityResults(searchTerm) {
+  coordinatesUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${searchTerm}&limit=5&APPID=${apiKey}`;
+  return fetch(coordinatesUrl).then(function (response) {
+    return response.json();
+  });
 }
 
-function fetchForecastUrl(latitude, longitude) {
-  var url =
-    "http://api.openweathermap.org/data/2.5/forecast?lat=" +
-    latitude +
-    "&lon=" +
-    longitude +
-    "&APPID=" +
-    apiKey;
-  return url;
-}
-
-function City(latitude, longitude) {
-  this.latitude = latitude;
-  this.longitude = longitude;
-}
-
-function getCoordinates(event) {
-  event.preventDefault();
-  fetch(fetchCoordinatesUrl($("#city-entry").val()))
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      getWeather(data);
-    });
-}
-
-function getWeather(data) {
-  fetch(fetchForecastUrl(data[0].lat, data[0].lon))
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
+// Event listener on search input change
+$("#location-search").on("input", function () {
+  inputValue = $(this).val();
+  if (inputValue.length > 2) {
+    fetchCityResults(inputValue).then(function (data) {
       console.log(data);
+      for (let i = 0; i < data.length; i++) {
+        createSearchOption(
+          data[i].name,
+          data[i].state,
+          data[i].country,
+          data[i].lat,
+          data[i].lon
+        );
+      }
     });
+  }
+});
+
+function createSearchOption(name, state, country, latitude, longitude) {
+  let option = `<option value="${name}, ${state}, ${country}" data-lat="${latitude}" data-lon="${longitude}"></option>`;
+  $("#location-options").append(option);
 }
 
-$("#city-search-btn").click(getCoordinates);
+// ---------------------
+// function fetchCoordinatesUrl(city) {
+//   url =
+//     "http://api.openweathermap.org/geo/1.0/direct?q=" +
+//     city +
+//     "&APPID=" +
+//     apiKey;
+//   return url;
+// }
+
+// function fetchForecastUrl(latitude, longitude) {
+//   var url =
+//     "http://api.openweathermap.org/data/2.5/forecast?lat=" +
+//     latitude +
+//     "&lon=" +
+//     longitude +
+//     "&APPID=" +
+//     apiKey;
+//   return url;
+// }
+
+// function getCoordinates(event) {
+//   fetch(fetchCoordinatesUrl($("#city-entry").val()))
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       getWeather(data);
+//     });
+// }
+
+// function getWeather(data) {
+//   fetch(fetchForecastUrl(data[0].lat, data[0].lon))
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//     });
+// }
+
+// $("#city-search-btn").click(getCoordinates);
